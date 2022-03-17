17f8b7873c349abcfa4e601ec03de3b5a538e1f1
Arthur Kepler <kepler@partavate.com>
2022-03-16 18:20:26 -0700

    March 2022 static PHP site

---
+++ about-us.php
@@ -0,0 +1,38 @@
+<div id="about" class=" pt-4 pb-4">
+  <div class="row p-4">
+
+    <h2 class="pt-4 text-white">About Us</h2>
+
+    <div class="col-md-2 col-sm-1 col-lg-2 col-xl-3">
+    </div>
+    <div class="col-md-8 col-sm-10 col-lg-8 col-xl-6">
+      <p class="">
+        Partavate Studios is an independent studio<br> creating interactive media using emerging technology.
+      </p>
+      <div class="row d-flex align-items-end">
+        <div class="col-md-4">
+
+          <img src="photo-koda.png" class="pl-4 pr-4 img-fluid" style="border-radius: 200px; border: 2px solid #ff88ddaa">
+          <div class="pt-2">Command</div>
+          <b class="text-white">Scott Dudley</b><br>
+          <small>Founder 0x0</small>
+
+        </div>
+        <div class="col-md-4">
+          <img src="photo-kepler.png" class="pl-4 pr-4 img-fluid" style="border-radius: 200px; border: 2px solid #ff88ddaa">
+          <div class="pt-2">Engineering</div>
+          <b class="text-white">Arthur Kepler</b><br>
+           <small>Cofounder 0x1</small>
+        </div>
+        <div class="col-md-4">
+          <img src="photo-sean.png" class="pl-4 pr-4 img-fluid" style="border-radius: 200px; border: 2px solid #ff88ddaa">
+          <div class="pt-2">Operations</div>
+          <b class="text-white">Sean Patchen</b><br>
+          <small>Cofounder 0x2</small>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 col-sm-1 col-lg-2 col-xl-3">
+    </div>
+  </div>
+</div>
---
+++ contact.php
@@ -0,0 +1,23 @@
+<div id="contact" class="pt-4 pb-4">
+  <div class="row p-4">
+
+    <h2 class="pt-4 text-white">Contact Us</h2>
+
+    <div class="col-xs-1 col-sm-1 col-md-2 col-lg-1 col-xl-2">
+    </div>
+
+    <div class="col-xs-10 col-sm-10 col-md-8 col-lg-10 col-xl-8">
+      <p>
+        For news, announcements, and community engagements, checkout <a href="https://twitter.com/partavate">Partavate's Twitter</a> and
+        <a href="https://discord.gg/DQaQWxE">Orbiter 8 Discord channel</a>. For investor relations, press inquiries, or collaborations,
+        <a href="mailto:info@partavate.com">email us</a>.
+      </p>
+      <p>
+      </p>
+    </div>
+
+    <div class="col-xs-1 col-sm-1 col-md-2 col-lg-1 col-xl-2">
+    </div>
+
+  </div>
+</div>
